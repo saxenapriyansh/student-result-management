dd6f2b2c37193ce1348543f9bf31b41dca7a58b8
llcodeAlphall <saxenapriyanshasd@gmail.com>
2018-10-24 00:36:03 +0530

    fixed logo

--- includes/leftbar.php
+++ includes/leftbar.php
@@ -1,9 +1,9 @@
 <div class="left-sidebar bg-black-300 box-shadow ">
                         <div class="sidebar-content">
                             <div class="user-info closed">
-                                <img src="http://placehold.it/90/c2c2c2?text=User" alt="John Doe" class="img-circle profile-img">
-                                <h6 class="title">John Doe</h6>
-                                <small class="info">PHP Developer</small>
+                                <img src="images/iiitm_logo.png" alt="ABV-IIITM" height="52" width="52" class="img-circle profile-img">
+                                <h6 class="title">ABV-IIITM</h6>
+                                <small class="info">Admin privilidges</small>
                             </div>
                             <!-- /.user-info -->
 
@@ -14,7 +14,7 @@
                                     </li>
                                     <li>
                                         <a href="dashboard.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span> </a>
-                                     
+
                                     </li>
 
                                     <li class="nav-header">
@@ -25,7 +25,7 @@
                                         <ul class="child-nav">
                                             <li><a href="create-class.php"><i class="fa fa-bars"></i> <span>Create Class</span></a></li>
                                             <li><a href="manage-classes.php"><i class="fa fa fa-server"></i> <span>Manage Classes</span></a></li>
-                                           
+
                                         </ul>
                                     </li>
   <li class="has-children">
@@ -42,7 +42,7 @@
                                         <ul class="child-nav">
                                             <li><a href="add-students.php"><i class="fa fa-bars"></i> <span>Add Students</span></a></li>
                                             <li><a href="manage-students.php"><i class="fa fa fa-server"></i> <span>Manage Students</span></a></li>
-                                           
+
                                         </ul>
                                     </li>
 <li class="has-children">
@@ -50,10 +50,10 @@
                                         <ul class="child-nav">
                                             <li><a href="add-result.php"><i class="fa fa-bars"></i> <span>Add Result</span></a></li>
                                             <li><a href="manage-results.php"><i class="fa fa fa-server"></i> <span>Manage Result</span></a></li>
-                                           
+
                                         </ul>
                                         <li><a href="change-password.php"><i class="fa fa fa-server"></i> <span> Admin Change Password</span></a></li>
-                                           
+
                                     </li>
                             </div>
                             <!-- /.sidebar-nav -->
